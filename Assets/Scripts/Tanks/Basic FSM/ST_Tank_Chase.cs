a93d036e8dfcf72df1276536695fce288715ff91
p1k11 <159910125+p1k11@users.noreply.github.com>
2024-12-09 10:57:14 +0000

    Revert "yayyyyy tank moves and tracks"
    This reverts commit 97b52ea4003405e8bd98ccd5cc34572040fabe2d.

--- Assets/Scripts/Tanks/Basic FSM/ST_Tank_Chase.cs
+++ Assets/Scripts/Tanks/Basic FSM/ST_Tank_Chase.cs
@@ -10,20 +10,13 @@ public class ST_Tank_Chase : ST_BaseTankState
     public override Type EnterState()
     {
         targetEnemy = null;
-<<<<<<< Updated upstream
         return null;
-=======
-        Debug.Log("in chase mode");
-        return null;
-        
->>>>>>> Stashed changes
     }
 
     // Called when leaving the state
     public override Type LeaveState()
     {
         targetEnemy = null;
-<<<<<<< Updated upstream
         return null;
     }
 
@@ -48,46 +41,6 @@ public override Type StateLogic()
 
     
     return null;
-=======
-        Debug.Log("exit chase mode");
-        return null;
-        
-    }
-
-    
-// Runs every physics update inside the controller
-float timer;
-
-public override Type StateLogic()
-{
-    // Get the dictionary of visible enemy tanks.
-    var visibleEnemies = tank.VisibleEnemyTanks;
-
-    if (visibleEnemies.Count > 0)
-    {
-        timer = 5;
-        // Move towards the enemy.
-        tank.enemyLastSeen.position = tank.VisibleEnemyTanks.Keys.First().transform.position;
-        tank.FollowPathToWorldPoint(tank.enemyLastSeen.gameObject, normalizedSpeed: 1.0f);
-        
-
-        
-        return typeof(ST_Tank_Chase);
-    }
-    else if (timer > 0){
-        timer -= Time.deltaTime;
-            tank.FollowPathToWorldPoint(tank.enemyLastSeen.gameObject, normalizedSpeed: 1.0f);
-            return null;
-        }
-    else
-    {
-        
-        tank.GenerateNewRandomWorldPoint();
-
-        
-        return typeof(ST_Tank_Search); 
-    }
->>>>>>> Stashed changes
 }
 
 
