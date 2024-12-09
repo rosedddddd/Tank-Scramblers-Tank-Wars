381285f33c0900bde0220d6b2ff2a643f346e575
p1k11 <159910125+p1k11@users.noreply.github.com>
2024-12-09 11:40:47 +0000

    version control issues im tweaking

--- Assets/Scripts/StateMachineClasses/ST_Controller.cs
+++ Assets/Scripts/StateMachineClasses/ST_Controller.cs
@@ -70,11 +70,7 @@ public class ST_Controller : MonoBehaviour
 
         if (curState == null)
         {
-<<<<<<< Updated upstream
-            curState = typeof(ST_Tank_Chase);
-=======
             curState = typeof(ST_Tank_Search);
->>>>>>> Stashed changes
         }
 
         
