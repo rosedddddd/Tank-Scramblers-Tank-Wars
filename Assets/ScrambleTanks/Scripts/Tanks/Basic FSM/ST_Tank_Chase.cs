35aec7ba1edbcc9b7c2d9dab89976bb2d6c664fa
p1k11 <159910125+p1k11@users.noreply.github.com>
2024-12-11 14:36:41 +0000

    even better more scary chase
    slay chase checks distance and consumables and wins

--- Assets/ScrambleTanks/Scripts/Tanks/Basic FSM/ST_Tank_Chase.cs
+++ Assets/ScrambleTanks/Scripts/Tanks/Basic FSM/ST_Tank_Chase.cs
@@ -21,37 +21,45 @@ public class ST_Tank_Chase : ST_BaseTankState
         return null;
         Debug.Log("exit chase mode");
     }
-
-    
-
-float timer;
-// Runs every physics update inside the controller
-public override Type StateLogic()
-{
-    // Get the dictionary of visible enemy tanks.
-    var visibleEnemies = tank.VisibleEnemyTanks;
-    float enemyDist = Vector3.Distance(tank.transform.position, tank.enemyLastSeen.transform.position);
-    
-
-    if (visibleEnemies.Count > 0 && enemyDist > 34)
+        
+    // Runs every physics update inside the controller
+    public override Type StateLogic()
     {
-        // Move towards the enemy.
-        tank.FollowPathToWorldPoint(visibleEnemies.Keys.First(), normalizedSpeed: 1.0f);//moving towards the visible enemy
-        tank.TurretFaceWorldPoint(tank.enemyLastSeen.gameObject);//pointing turret at enemy
-        timer -= Time.deltaTime;
-        Debug.Log("chasing");
-        return typeof(ST_Tank_Chase);//stay in chase
+        // Get the dictionary of visible enemy tanks.
+        var visibleEnemies = tank.VisibleEnemyTanks;
+        float enemyDist = Vector3.Distance(tank.transform.position, tank.enemyLastSeen.transform.position);//looking at distance before switching to attack
+        
 
+        if (visibleEnemies.Count > 0 && enemyDist > 35 || tank.VisibleConsumables.Count > 0)
+        {
+            if (enemyDist < 35)
+            {
+                tank.FollowPathToWorldPoint(visibleEnemies.Keys.First(), normalizedSpeed: 1.0f);//moving towards the visible enemy
+                tank.TurretFaceWorldPoint(tank.enemyLastSeen.gameObject);//pointing turret at enemy
+                Debug.Log("chasing enemy");
+                return typeof(ST_Tank_Chase);//stay in chase
 
+            }
+            else if (tank.VisibleConsumables.Count > 0)
+            {
+                tank.FollowPathToWorldPoint(tank.VisibleConsumables.Keys.First(), normalizedSpeed: 1.0f);//moving towards the visible consumable prioritising
+                return typeof(ST_Tank_Chase);
+                
+
+            }
+
+        }
+        else
+        {
+            Debug.Log("close enough switching to attack");
+            return typeof(ST_Tank_Attack);//when timer runs out tank attacks
+                
+        }
+        return null;
+        
     }
-    else
-    {
-        Debug.Log("close enough switching to attack");
-        return typeof(ST_Tank_Attack);//when timer runs out tank attacks
-            
-    }
-       
+    
+        
 }
 
 
-}
